a6da0fe762fbc97a49afdc14017f01e071011359
tux <tux@tux.tux.com>
2023-11-17 20:28:51 -0300

    init

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import { Hono } from "hono";
+import Replicate from "replicate";
+import { cors } from 'hono/cors'
+
+const app = new Hono();
+
+app.use('/api/*', cors())
+
+const replicate = new Replicate({
+  auth: "r8_S9pbxWXNsQB0alSjQEpFa5RYQRauNPT0tZZK7",
+});
+
+app.get("/api", (c) => c.text("Welcome to Summarize.ai API"));
+
+app.post("/api/summarize", async (c) => {
+  const body = await c.req.json();
+  const output = await replicate.run(
+    "meta/llama-2-70b-chat:02e509c789964a7ea8736978a43525956ef40397be9033abf9fd2badfe68c9e3",
+    {
+      input: {
+        prompt: `Please summarize the following article: ${body.url}`,
+      },
+    }
+  );
+  return c.json(output);
+});
+
+export default app;
